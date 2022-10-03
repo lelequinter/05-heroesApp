f8ecb58d0a5bf922aa51de2f1031b36239d7f084
Leyder Quintero <lele.quinter@gmail.com>
2022-10-03 17:43:37 -0500

    Material Sidenav, Toolbar e iconos

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '../material/material.module';
 
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
@@ -21,7 +22,8 @@ import { ListadoComponent } from './pages/listado/listado.component';
   imports: [
     CommonModule,
     HeroesRoutingModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MaterialModule
   ]
 })
 export class HeroesModule { }
