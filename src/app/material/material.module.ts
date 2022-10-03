95c8c097216b0e52e60a58b43a21263e4f18ed04
Leyder Quintero <lele.quinter@gmail.com>
2022-10-03 18:03:01 -0500

    Material Navlist

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,6 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   exports: [
@@ -11,6 +12,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatIconModule,
     MatSidenavModule,
     MatToolbarModule,
+    MatListModule,
   ]
 })
 export class MaterialModule { }
