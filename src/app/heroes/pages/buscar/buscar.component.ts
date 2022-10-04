76591aa6adb081f273f7b18eaaabc2b4847fa76a
Leyder Quintero <lele.quinter@gmail.com>
2022-10-04 09:28:04 -0500

    Autocomplete - Segunda Parte

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { Heroe } from '../../interface/heroe.interface';
 import { HeroesService } from '../../services/heroes.service';
 
@@ -12,6 +13,8 @@ export class BuscarComponent implements OnInit {
 
   termino: string = '';
   heroes: Heroe[] = [];
+  heroeSeleccionado!: Heroe;
+
 
   constructor(private heroesService: HeroesService) { }
 
@@ -19,8 +22,16 @@ export class BuscarComponent implements OnInit {
   }
 
   buscando() {
-    this.heroesService.getHeroes()
+    this.heroesService.getSugerencias(this.termino)
       .subscribe(heroes => this.heroes = heroes)
+  }
+
+  opcionSeleccionada(event: MatAutocompleteSelectedEvent) {
+    const heroe: Heroe = event.option.value;
+    this.termino = heroe.superhero;
+    // console.log(heroe);
+    this.heroesService.getHeroePorId(heroe.id!)
+      .subscribe(heroe => this.heroeSeleccionado = heroe)
   }
 
 }
