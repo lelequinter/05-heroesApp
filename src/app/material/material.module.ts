dbde00a7b6f775d1c894ac93baa673d0db6bd799
Leyder Quintero <lele.quinter@gmail.com>
2022-10-03 20:32:41 -0500

    Material Card - Flex Layout

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   exports: [
     MatButtonModule,
+    MatCardModule,
     MatIconModule,
+    MatListModule,
     MatSidenavModule,
     MatToolbarModule,
-    MatListModule,
   ]
 })
 export class MaterialModule { }
