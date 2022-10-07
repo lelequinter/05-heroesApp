ab12e08eb37c363823048b0bd13dbb73c022bfe9
Leyder Quintero <lele.quinter@gmail.com>
2022-10-06 20:52:42 -0500

    Mantener la sesión del usuario

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,34 +1,56 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate, CanLoad {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService,
+    private router: Router) { }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
 
-      if (this.authService.auth.id) {
-        return true;
-      }
+    return this.authService.verificaAutenticacion()
+      .pipe(
+        tap(estaAutenticado => {
+          if (!estaAutenticado) {
+            this.router.navigate(['/auth/login'])
+          }
+        })
+      );
 
-    return true;
+    //   if (this.authService.auth.id) {
+    //     return true;
+    //   }
+
+    // return true;
   }
+
+
 
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
 
-    if (this.authService.auth.id) {
-      return true;
-    }
+    return this.authService.verificaAutenticacion()
+      .pipe(
+        tap(estaAutenticado => {
+          if (!estaAutenticado) {
+            this.router.navigate(['/auth/login'])
+          }
+        })
+      );
 
-    return false;
+    // if (this.authService.auth.id) {
+    //   return true;
+    // }
+
+    // return false;
   }
 }
